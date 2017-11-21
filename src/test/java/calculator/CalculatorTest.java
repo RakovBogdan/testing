904649a50de0e14d4501352f251bdc139a8ebfb5
RakovBogdan <rakovbogdan@gmail.com>
2017-11-21 13:55:56 +0200

    Calculator Test Implemented

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import static calculator.domain.Service.*;
 import calculator.domain.ComplexObject;
 import calculator.service.CountingService;
 import calculator.service.FieldValidationService;
@@ -27,29 +28,106 @@ public class CalculatorTest {
     @InjectMocks
     private Calculator calculator;
 
+    @Test(expected = IllegalArgumentException.class)
+    public void complexObjectsWithDifferentServicesTest() {
+        ComplexObject objectA = new ComplexObject(1, 2, APPHE);
+        ComplexObject objectB = new ComplexObject(3, 4, CAMSUNG);
+
+        Mockito.doNothing().when(validationService).validateFields(objectA);
+        Mockito.doNothing().when(validationService).validateFields(objectB);
+        Mockito.doNothing().when(statisticsService).incrementAdditionCount();
+
+        calculator.add(objectA, objectB);
+    }
+
+    public void additionUpdatesMethodCallsCounter() {
+        ComplexObject objectA = new ComplexObject(1, 2, APPHE);
+        ComplexObject objectB = new ComplexObject(3, 4, CAMSUNG);
+
+        Mockito.doNothing().when(validationService).validateFields(objectA);
+        Mockito.doNothing().when(validationService).validateFields(objectB);
+
+        calculator.add(objectA, objectB);
+    }
+
     @Test
     public void addTest() {
-        //Given
-        ComplexObject objectA = new ComplexObject(1, 2);
-        ComplexObject objectB = new ComplexObject(3, 4);
 
-        ComplexObject expectedResult = new ComplexObject(0,0);
+        ComplexObject objectA = new ComplexObject(1, 2, APPHE);
+        ComplexObject objectB = new ComplexObject(3, 4, APPHE);
+
+        ComplexObject expectedResult = new ComplexObject(4,6);
+
+        Mockito.doNothing().when(validationService).validateFields(objectA);
+        Mockito.doNothing().when(validationService).validateFields(objectB);
+        Mockito.doNothing().when(statisticsService).incrementAdditionCount();
 
         Mockito.when(countingService.add(objectA, objectB)).thenReturn(expectedResult);
 
-        //When
         ComplexObject result = calculator.add(objectA, objectB);
 
-        //Then
         Assert.assertEquals(expectedResult, result);
-        Mockito.verify(countingService, Mockito.times(1))
-                .add(objectA, objectB);
-//        Mockito.reset(countingService);
     }
 
     @Test
-    public void statisticsAdditionsTest() {
-        ComplexObject objectA = new ComplexObject(1, 2);
-        ComplexObject objectB = new ComplexObject(3, 4);
+    public void subtractTest() {
+
+        ComplexObject objectA = new ComplexObject(3, 2, APPHE);
+        ComplexObject objectB = new ComplexObject(3, 4, APPHE);
+
+        ComplexObject expectedResult = new ComplexObject(0,-2);
+
+        Mockito.doNothing().when(validationService).validateFields(objectA);
+        Mockito.doNothing().when(validationService).validateFields(objectB);
+        Mockito.doNothing().when(statisticsService).incrementAdditionCount();
+
+        Mockito.when(countingService.subtract(objectA, objectB)).thenReturn(expectedResult);
+
+        ComplexObject result = calculator.subtract(objectA, objectB);
+
+        Assert.assertEquals(expectedResult, result);
     }
+
+    @Test
+    public void multiplyTest() {
+
+        ComplexObject objectA = new ComplexObject(5, 2, APPHE);
+        ComplexObject objectB = new ComplexObject(3, 4, APPHE);
+
+        ComplexObject expectedResult = new ComplexObject(15,8);
+
+        Mockito.doNothing().when(validationService).validateFields(objectA);
+        Mockito.doNothing().when(validationService).validateFields(objectB);
+        Mockito.doNothing().when(statisticsService).incrementAdditionCount();
+
+        Mockito.when(countingService.multiply(objectA, objectB)).thenReturn(expectedResult);
+
+        ComplexObject result = calculator.multiply(objectA, objectB);
+
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void divideTest() {
+
+        ComplexObject objectA = new ComplexObject(8, 10, APPHE);
+        ComplexObject objectB = new ComplexObject(2, 5, APPHE);
+
+        ComplexObject expectedResult = new ComplexObject(4,2);
+
+        Mockito.doNothing().when(validationService).validateFields(objectA);
+        Mockito.doNothing().when(validationService).validateFields(objectB);
+        Mockito.doNothing().when(statisticsService).incrementAdditionCount();
+
+        Mockito.when(countingService.divide(objectA, objectB)).thenReturn(expectedResult);
+
+
+        ComplexObject result = calculator.divide(objectA, objectB);
+
+
+        Assert.assertEquals(expectedResult, result);
+    }
+
+
+
 }
